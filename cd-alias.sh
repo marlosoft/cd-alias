c8414eba3a604adce19b6ec3cc79600ab72a410e
marlosoft <info@marlosoft.net>
2018-05-10 11:39:26 +0800

    Change subcommand names + Add alias with by name and path

--- cd-alias.sh
+++ cd-alias.sh
@@ -7,23 +7,26 @@ function cd() {
     local cfg="${path}/alias.cfg"
     local tmp="${path}/.alias.cfg.tmp"
 
-    [ -d "${path}" ] || mkdir -p ${path}
-    [ -f "${cfg}" ] || touch ${cfg}
+    [ ! -d "${path}" ] && mkdir -p ${path}
+    [ ! -f "${cfg}" ] && touch ${cfg}
 
-    if [ "${cmd}" == "help" ]; then
-        echo -e "\e[0;32mUSAGE:\e[0m"
+    if [ "${cmd}" == "--help" ]; then
+        echo -e "\e[0;32mUSAGE: (cd-alias)\e[0m"
         echo -e " - cd <path>                 Uses built-in cd command in bash"
-        echo -e " - cd alias <name>           Creates a named alias and uses the current directory as the path"
-        echo -e " - cd unalias <name>         Removes created alias having the name argument"
-        echo -e " - cd ls                     Displays the list of current alias names and corresponding path"
-        echo -e " - cd help                   Displays this message (usages)"
+        echo -e " - cd --add <name>           Creates a named alias and uses the current directory as the path"
+        echo -e " - cd --add <name> <path>    Creates a named alias and using the next argument as path       "
+        echo -e " - cd --remove <name>        Removes created alias having the name argument                  "
+        echo -e " - cd --list                 Displays the list of current alias names and corresponding path "
+        echo -e " - cd --help                 Displays help message (cd-alias usages and built-in cd help)    "
+        echo -e "---------------------------------------------------------------------------------------------"
+        builtin cd --help
         return 0
-    elif [ "${cmd}" == "alias" ]; then
+    elif [ "${cmd}" == "--add" ]; then
         if [ -z "$2" ]; then
             echo -e "\e[0;31mERROR:\e[0m \e[0;33m<name>\e[0m argument value required"
             return 1
         elif ! [[ "$2" =~ ^([A-Za-z])([A-Za-z0-9-]+)([A-Za-z0-9])$ ]]; then
-            echo -e "\e[0;31mERROR:\e[0m \e[0;33m<name>\e[0m argument invalid format \e[0;32m(alnum)\e[0m"
+            echo -e "\e[0;31mERROR:\e[0m \e[0;33m<name>\e[0m argument invalid format \e[0;32m(alnum, n>3)\e[0m"
             return 1
         fi
 
@@ -34,11 +37,17 @@ function cd() {
             return 0
         fi
 
-        local _path=$(pwd)
+        local _path=$(pwd -P)
+        [ -n "$3" ] && _path=$(cd $3 &>/dev/null && pwd -P || echo "")
+        if [ -z "${_path}" ]; then
+            echo -e "\e[0;31mERROR:\e[0m path or directory \"\e[0;33m$3\e[0m\" does not exists"
+            return 1
+        fi
+
         echo "$2=${_path}" >> ${cfg}
         echo -e "\e[0;32mINFO:\e[0m create alias \"\e[0;33m$2\e[0m\" (\e[0;32m${_path}\e[0m)"
         return 0
-    elif [ "${cmd}" == "unalias" ]; then
+    elif [ "${cmd}" == "--remove" ]; then
         local alias=$(grep "^$2\=" ${cfg})
         if [ -z "${alias}" ]; then
             echo -e "\e[0;31mERROR:\e[0m alias name \"\e[0;33m$2\e[0m\" does not exists"
@@ -50,7 +59,7 @@ function cd() {
         mv ${tmp} ${cfg}
         echo -e "\e[0;32mINFO:\e[0m removed alias \"\e[0;33m$2\e[0m\""
         return 0
-    elif [ "${cmd}" == "list" ]; then
+    elif [ "${cmd}" == "--list" ]; then
         local aliases=$(cat ${cfg})
         if [ -z "${aliases}" ]; then
             echo -e "\e[0;35mWARN:\e[0m no aliases found"
